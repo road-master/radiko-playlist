40e73822197bf1b85f8bfbd4ffc1c21290e38072
Master <roadmasternavi@gmail.com>
2020-05-30 19:38:15 +0900

    Bump version: 0.1.0 → 1.0.0

--- setup.py
+++ setup.py
@@ -35,6 +35,6 @@ setup(
     test_suite="tests",
     tests_require=["pytest>=3"],
     url="https://github.com/road-master/radiko-playlist",
-    version="0.1.0",
+    version="1.0.0",
     zip_safe=False,
 )
