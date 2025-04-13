c58f5841fb0f5b7a33dfad9b61a98f5c1685bcf0
Master <roadmasternavi@gmail.com>
2025-04-14 04:32:41 +0900

    Bump version: 1.1.0 → 1.1.1

--- radikoplaylist/__init__.py
+++ radikoplaylist/__init__.py
@@ -5,7 +5,7 @@ from radikoplaylist.master_playlist_request import *  # noqa: F403
 
 __author__ = """Master"""
 __email__ = "roadmasternavi@gmail.com"
-__version__ = "1.1.0"
+__version__ = "1.1.1"
 
 __all__ = []
 __all__ += master_playlist_request.__all__  # type:ignore[name-defined] # noqa: F405 pylint: disable=undefined-variable
